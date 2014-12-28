ebdf42c8854a5c4ddbdb6eedef7a04054ff15f67
Alejandro Londonogit config --global user.email alondono11@gmail.comcdgit config --;istgit config --global user.name Alejandro <alondono11@gmail.com>
2014-12-27 21:57:28 -0500

    updated js files, renamed gallery.html to index.html

--- js/the-gallery.js
+++ js/the-gallery.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 //when page is loaded, 'the gallery should fade into view'
 $('.pink').hide().fadeIn(3000);
+$('.number').hide();
 
 $(window).scroll(function(){
 //when the page scroll is engaged, change the opacity of the nav bar
@@ -10,7 +11,12 @@ if ($('.navbar').offset().top > 10){
 }	else{
 		$(".navbar-the-gallery").removeClass("top-nav-collapse");
 				}
-			});
+	});
+$('.contact').click(function(event){
+	event.preventDefault();
+	$('.number').slideToggle();
+});
+
 
 });
 
